eed77d2361d0a2f880e4512a43a65a8dd7c9140e
usamashabir54321 <usamashabir54321@gmail.com>
2021-10-08 13:16:45 +0500

    with loader

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,37 @@
 <template>
 	<div>
-		<section class="showcase" :class="[ sideBar ? 'active' : '' ]">
-			<header>
-				<h2 class="logo">Travel</h2>
-				<div :class="[ sideBar ? 'active' : '' ]" class="toggle" @click="sideBar = !sideBar"></div>
-			</header>
-			<div v-for="(vd , index) in vids" :key="index">
-				<video v-show="index == selectVid" muted loop autoplay><source :src="`https://attakus.masoodapp.com//video/${vd.src}.mp4`" type="video/mp4"></video>
+		<div v-show="showLoadr" class="loading"><span></span></div>
+		<div v-show="!showLoadr">
+			<section class="showcase" :class="[ sideBar ? 'active' : '' ]">
+				<header>
+					<h2 class="logo">Travel</h2>
+					<div :class="[ sideBar ? 'active' : '' ]" class="toggle" @click="sideBar = !sideBar"></div>
+				</header>
+				<div v-for="(vd , index) in vids" :key="index">
+					<video v-show="index == selectVid" muted loop autoplay><source :src="`https://attakus.masoodapp.com//video/${vd.src}.mp4`" type="video/mp4"></video>
+				</div>
+				<div class="overlay"></div>
+				<div class="text">
+					<h2>Never Stop To </h2> 
+					<h3>Exploring The World</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+					<a>Explore {{ selectVid+1 }}</a>
+				</div>
+				<ul class="social">
+					<li><span @click="selectVid -= 1">&#8592;</span></li>
+					<li v-for="(vd , index) in vids" :key="index"><span :class="[ index == selectVid ? 'active' : '' ]" @click="selectVid = index"></span></li>
+					<li><span @click="selectVid += 1">&#8594;</span></li>
+				</ul>
+			</section>
+			<div class="menu">
+				<ul>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">News</a></li>
+					<li><a href="#">Destination</a></li>
+					<li><a href="#">Blog</a></li>
+					<li><a href="#">Contact</a></li>
+				</ul>
 			</div>
-			<div class="overlay"></div>
-			<div class="text">
-				<h2>Never Stop To </h2> 
-				<h3>Exploring The World</h3>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-				<a>Explore {{ selectVid+1 }}</a>
-			</div>
-			<ul class="social">
-				<li><span @click="selectVid -= 1">&#8592;</span></li>
-				<li v-for="(vd , index) in vids" :key="index"><span :class="[ index == selectVid ? 'active' : '' ]" @click="selectVid = index"></span></li>
-				<li><span @click="selectVid += 1">&#8594;</span></li>
-			</ul>
-		</section>
-		<div class="menu">
-			<ul>
-				<li><a href="#">Home</a></li>
-				<li><a href="#">News</a></li>
-				<li><a href="#">Destination</a></li>
-				<li><a href="#">Blog</a></li>
-				<li><a href="#">Contact</a></li>
-			</ul>
 		</div>
 	</div>
 </template>
@@ -40,7 +43,11 @@
 				sideBar: false,
 				vids: [],
 				selectVid: 0,
+				showLoadr: true,
 			}
+		},
+		async fetch() {
+			await this.myImgArrs();
 		},
 		watch: {
 			selectVid (val) {
@@ -48,8 +55,8 @@
 				else if (val < 0) { this.selectVid = 0 }
 			},
 		},
-		async fetch() {
-			await this.myImgArrs();
+		created () {
+			setTimeout(() => { this.showLoadr = false; },1000);
 		},
 		fetchDelay: 1000,
 		methods: {
